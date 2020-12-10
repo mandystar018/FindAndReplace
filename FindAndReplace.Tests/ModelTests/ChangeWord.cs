ae44a805ec8abebd59c66402ffc95a0feb8e9032
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-10 12:01:43 -0800

    Test create instance purposefully failed
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

---
+++ FindAndReplace.Tests/ModelTests/ChangeWord.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindAndReplace.Models;
+
+namespace FindAndReplace.Tests
+{
+  [TestClass]
+  public class ChangeWordTests //: IDisposable
+  {
+    // public void Dispose()
+    // {
+    //   ChangeWord.ClearAll();
+    // }
+
+    [TestMethod]
+    public void ChangeWordConstructor_CreatesInstanceOfChangeWord_ChangeWord()
+    {
+      ChangeWord newList = new ChangeWord("Hello World", "word", "universe");
+      Assert.AreEqual(typeof(ChangeWord), newList);
+    }
+  }
+}
+
+
+
